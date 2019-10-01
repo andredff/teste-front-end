fdff70c6949904a00885482243a1a030f29a4ed5
Andre Souza <andre.souza@solucioneonline.com.br>
2019-09-30 22:20:12 -0300

    creating home component

---
+++ searchTube/src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  @ViewChild('videos', {static: false}) public videos: any;
+
+  @ViewChild('search', {static: false}) public search: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  atualizarVideos(term){
+    this.videos.getVideos(term);
+  }
+
+  clear(){
+    this.videos.clearCache();
+  }
+
+  atualizarEstado(value) {
+    this.search.atualizarEstado(value)
+  }
+
+}
