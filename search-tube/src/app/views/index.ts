3bdb0f68595345b625dfa3e751f66ab629ec7c63
Andre Souza <andre.souza@solucioneonline.com.br>
2019-10-01 11:54:45 -0300

    .

--- search-tube/src/app/views/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './home/search/search.component';
-export * from './home/home.component';
-export * from './home/video/video.component';
-// export * from './home/video/details/details.component';
